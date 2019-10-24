2b031f6d844fe93ae5f21cde3c331a064009fef9
bhavey15 <56358885+bhavey15@users.noreply.github.com>
2019-10-25 02:26:49 +0530

    Update server.c

--- server.c
+++ server.c
@@ -9,4 +9,7 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]){
+  int sock= socket(AF_NET, SOCK_STREAM, 0);
+  struct sockaddr_in server;
+  server.
