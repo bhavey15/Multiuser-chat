bc7095fc82a3b2b79cfcb2f990201f166fcd1d48
bhavey15 <56358885+bhavey15@users.noreply.github.com>
2019-10-25 02:21:11 +0530

    Create server.c

---
+++ server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+int main(int argc, char* argv[])
