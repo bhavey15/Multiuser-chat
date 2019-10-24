77d699d3360affaeb096fe9fcb860eee513f78d1
bhavey15 <56358885+bhavey15@users.noreply.github.com>
2019-10-24 21:44:07 +0530

    Update client.c

--- client.c
+++ client.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
