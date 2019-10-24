d80d7f06f84f943c0f9293d6e86daa2923385b65
bhavey15 <56358885+bhavey15@users.noreply.github.com>
2019-10-25 02:16:54 +0530

    Update client.c

--- client.c
+++ client.c
@@ -8,3 +8,46 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
+char msg[500];
+void* receive(void* args){
+  char data[1024];
+  int clientsocket= *((int *)args);
+  while(1){
+    int r_val=recv(clientsocket, data, 1024, 0);
+    data[r_val]='\0';
+    printf("%s\n", data);
+  }
+}
+
+int main(int argc, char* argv[]){
+  int clientsocket= socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in sket;
+  sket.sin_family = AF_NET;
+  sket.sin_addr.s_addr = htonl(INADDR_ANY);
+  sket.set_port = htons(1550);
+  char client_name[500];
+  strcpy(client_name, argv[1]);
+  if(connect(clientsocket, (sockaddr*) &sket, sizeof(sket))<0){
+    printf("Connection did not establish.. exiting");
+    return 0;
+  }
+  pthread_t thread;
+  pthread_create(&thread, NULL, (void *)receive, &sket);
+  while(1){
+    char s_msg[500];
+    scanf("%s", msg);
+    strcpy(s_msg, client_name);
+    strcat(s_msg, ":");
+    strcat(s_msg, msg);
+    int s= send(clientsocket, s_msg, 1024,0);
+    if(s<0){
+      printf("message not sent \n");
+    }
+  }
+  pthread_join(thread, NULL);
+  close(clientsocket);
+}
+
+    
+  
+    
